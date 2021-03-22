447f8ffc4e9be74918fcf6a32345bff90097f500
Reyk Floeter <contact@reykfloeter.com>
2021-03-22 11:13:10 +0100

    Move main.rs to a test

--- privsep/tests/imsg.rs
+++ privsep/tests/imsg.rs
@@ -1,4 +1,3 @@
-use futures::{channel::mpsc, SinkExt, StreamExt};
 use privsep::{imsg, net::Fd};
 use serde_derive::{Deserialize, Serialize};
 use std::{io, net::TcpListener, os::unix::io::IntoRawFd, time::Duration};
@@ -10,75 +9,58 @@ struct Message {
     name: String,
 }
 
-#[tokio::main]
-async fn main() -> Result<(), io::Error> {
-    unix_channel().await?;
-    main_channel().await
+#[tokio::test(flavor = "multi_thread")]
+async fn test_imsg() -> Result<(), io::Error> {
+    unix_channel().await
 }
 
 async fn unix_channel() -> Result<(), std::io::Error> {
     let (sender, receiver) = imsg::Handler::pair()?;
+    let mut count = 3;
 
     tokio::spawn(async move {
-        let mut interval = interval(Duration::from_millis(1));
+        let mut interval = interval(Duration::from_millis(100));
 
         let fd = TcpListener::bind("127.0.0.1:1234")
             .ok()
             .map(|stream| stream.into_raw_fd())
             .map(Fd::from);
 
-        for id in 1..=3 {
+        for id in 1..=count {
             interval.tick().await;
 
             let message = Message {
                 id,
-                name: "foo".to_string(),
+                name: "test".to_string(),
             };
 
             if let Err(err) = sender
                 .send_message(1u32.into(), fd.as_ref(), &message)
                 .await
             {
-                eprintln!("Failed to send ping: {}", err);
+                eprintln!("Failed to send message: {}", err);
             }
         }
     });
 
-    loop {
+    let res = loop {
         match { receiver.recv_message::<Message>().await } {
             Ok(None) => break Ok(()),
             Ok(Some((imsg, fd, message))) => {
+                count -= 1;
                 println!(
-                    "Received ping: {:?}, fd: {:?}, data: {:?}",
+                    "Received message: {:?}, fd: {:?}, data: {:?}",
                     imsg, fd, message
                 );
             }
             Err(err) => {
-                eprintln!("Failed to receive ping: {}", err);
+                eprintln!("Failed to receive messafe: {}", err);
                 break Err(err);
             }
         }
-    }
-}
+    };
 
-async fn main_channel() -> Result<(), std::io::Error> {
-    let (mut sender, mut receiver) = mpsc::unbounded::<usize>();
+    assert!(count == 0, "did not receive expected messages");
 
-    tokio::spawn(async move {
-        let mut interval = interval(Duration::from_secs(1));
-
-        for i in 1..=3 {
-            interval.tick().await;
-
-            if let Err(err) = sender.send(i).await {
-                eprintln!("Failed to send ping: {}", err);
-            }
-        }
-    });
-
-    while let Some(ping) = receiver.next().await {
-        println!("Received ping: {}", ping);
-    }
-
-    Ok(())
+    res
 }
